72e5aa960c47ba72568f377e25d355285186e6c9
Adrien Siebert <asiebert@work4labs.com>
2014-08-08 13:54:54 +0200

    Move load_cloudflare_cookbook_gems to library

--- providers/threat_control.rb
+++ providers/threat_control.rb
@@ -24,18 +24,3 @@ action :remove_ip do
     new_resource.remove_ip
   end
 end
-
-private
-
-# this needs to be done at run time, not compile time
-def load_cloudflare_cookbook_gems
-  return if defined? @@cloudflare_cookbook_gems_loaded
-
-  chef_gem 'cloudflare' do
-    action :nothing
-    version '2.0.1'
-  end.run_action(:install, :immediately)
-  require 'resolv'
-  require 'cloudflare'
-  @@cloudflare_cookbook_gems_loaded = true
-end
